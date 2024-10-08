176515871bca3e6064414c5d856df74cc1745f4e
jacqueline294 <jngigi86@gmail.com>
2024-10-08 13:32:31 +0200

    Initial Commit

---
+++ MyChatApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyChatApp
+//
+//  Created by jacqueline Ngigi on 2024-10-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyChatApp/MyChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyChatAppApp.swift
+//  MyChatApp
+//
+//  Created by jacqueline Ngigi on 2024-10-08.
+//
+
+import SwiftUI
+
+@main
+struct MyChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
