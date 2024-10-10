5e755f0fec739e06908e806c9e12ee3c2eb8e34f
jacqueline294 <144172392+jacqueline294@users.noreply.github.com>
2024-10-10 12:35:48 +0200

    adding the log in page and firbase

--- MyChatApp/LoginView.swift
+++ MyChatApp/LoginView.swift
@@ -6,18 +6,79 @@
 //
 
 import SwiftUI
+import Firebase
 
 struct LoginView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+    
+   @State var isLoginMode = false
+    @State var email = ""
+        @State var password = ""
+    
+    init(){
+        FirebaseApp.configure()
+        
     }
-}
+    
+    var body: some View {
+        // to be  able to move between views
+        NavigationView{
+            // to make the page  Scrollable
+            ScrollView{
+                
+                Picker(selection: $isLoginMode, label: Text("Picker here")) {
+                                        Text("Login")
+                                            .tag(true)
+                                        Text("Create Account")
+                                            .tag(false)
+                                    }.pickerStyle(SegmentedPickerStyle())
+                                        
+                                    if !isLoginMode {
+                                        Button {
+                                            
+                                        } label: {
+                                            Image(systemName: "person.fill")
+                                                .font(.system(size: 64))
+                                                .padding()
+                                        }
+                                    }
+               
+                Group {
+                                       TextField("Email", text: $email)
+                                           .keyboardType(.emailAddress)
+                                           .autocapitalization(.none)
+                                       SecureField("Password", text: $password)
+                                   }
+                                   .padding(12)
+                                   .background(Color.white)
+                                   
+                                   Button {
+                                       
+                                   } label: {
+                                       HStack {
+                                           Spacer()
+                                           Text(isLoginMode ? "Log In" : "Create Account")
+                                               .foregroundColor(.white)
+                                               .padding(.vertical, 10)
+                                               .font(.system(size: 14, weight: .semibold))
+                                           Spacer()
+                                       }.background(Color.blue)
+                                       
+                                   }
+                               }
+                               .padding()
+                               
+                
+
+                
+                Text("Here is my creation account page")
+                
+            }
+            .navigationTitle("Create Account")
+            
+        }
+        
+    }
+
 
 #Preview {
     LoginView()
